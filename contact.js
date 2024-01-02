42e448e1a497b24380a5612ad95f5e06294dde74
jjacklee <jjacklee@gist.ac.kr>
2024-01-02 18:04:33 +0900

    마음전할곳 만들기 시작

---
+++ contact.js
@@ -0,0 +1,23 @@
+function showContact() {
+    document.getElementById('ContactOverlay').style.display = 'block';
+}
+
+function closeContact() {
+    document.getElementById('ContactOverlay').style.display = 'none';
+}
+
+function collapse(element) {
+    var before = document.getElementsByClassName("active")[0]               // 기존에 활성화된 버튼
+    if (before && document.getElementsByClassName("active")[0] != element) {  // 자신 이외에 이미 활성화된 버튼이 있으면
+        before.nextElementSibling.style.maxHeight = null;   // 기존에 펼쳐진 내용 접고
+        before.classList.remove("active");                  // 버튼 비활성화
+    }
+    element.classList.toggle("active");         // 활성화 여부 toggle
+
+    var content = element.nextElementSibling;
+    if (content.style.maxHeight != 0) {         // 버튼 다음 요소가 펼쳐져 있으면
+        content.style.maxHeight = null;         // 접기
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";  // 접혀있는 경우 펼치기
+    }
+}
